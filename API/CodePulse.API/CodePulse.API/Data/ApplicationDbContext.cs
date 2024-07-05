46c331a91cd376ac43053b9ef0971aaf0c634ed0
Summy Singh <summysingh86@gmail.com>
2024-07-05 23:51:45 +0530

    API Code added

---
+++ API/CodePulse.API/CodePulse.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CodePulse.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodePulse.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //constructor for the DbContext
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //DbSet represents the collection of entities from a particular type, in a relational database
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; } 
+    }
+}
--- API/CodePulse.API/CodePulse.API/Program.cs
+++ API/CodePulse.API/CodePulse.API/Program.cs
@@ -1,3 +1,8 @@
+using CodePulse.API.Data;
+using CodePulse.API.Repositories.Implmentation;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Injecting connection string through service using DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CodePulseConnectionString"));
+});
+
+//injecting repositories ->Need Interface and Implementation class
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 var app = builder.Build();
 
