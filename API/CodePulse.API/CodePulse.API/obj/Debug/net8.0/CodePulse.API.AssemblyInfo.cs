9bac12015d9eda309c8cb56a5e3463bb78af41de
Summy Singh <summysingh86@gmail.com>
2024-07-07 01:22:13 +0530

    UI added
    UI Added with Adding Category

--- API/CodePulse.API/CodePulse.API/obj/Debug/net8.0/CodePulse.API.AssemblyInfo.cs
+++ API/CodePulse.API/CodePulse.API/obj/Debug/net8.0/CodePulse.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0994e97227c95ea67a9d4b0dde341de2651b6285")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46c331a91cd376ac43053b9ef0971aaf0c634ed0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodePulse.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
